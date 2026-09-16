d0444ca71cdc6c8b203f365c2a90f3d0122fc7f9
Diplal <diplalthakurbarhi@gmail.com>
2026-09-16 11:28:16 +0545

    Learned a few basics of c++

---
+++ CppTutorial/AccessStrings.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string myString = "Hello, C++!";
+    cout << myString[0];
+    cout << myString[1];
+    cout << myString[2];
+    cout << myString[3];
+    cout << myString[4];
+    
+// prinTing in a new line
+
+    cout << myString[0] << endl;
+    cout << myString[1] << endl;
+    cout << myString[2] << endl;
+    cout << myString[3] << endl;
+    cout << myString[4] << endl;
+    return 0;
+}
